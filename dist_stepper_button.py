098021df6451856cbc5868ef166eb6bb1f9be0b4
Matthew Adams <snimpids@gmail.com>
2020-10-16 21:33:37 -0600

    Used threading to wait for physical processes to be done. Ready for testing.

--- dist_stepper_button.py
+++ dist_stepper_button.py
@@ -1,104 +1,29 @@
 from time import sleep, time
 import RPi.GPIO as gpio
-
-pul = 17
-dir = 27
-ena = 22
-
-button_pin = 14
-
-trig = 18
-echo = 10
-
-motor_in1 = 23
-motor_in2 = 24
-en = 25
-temp1 = 1
+import physical_objects as ph
 
 gpio.setwarnings(False)
 gpio.setmode(gpio.BCM)
 
-gpio.setup(pul, gpio.OUT)
-gpio.setup(dir, gpio.OUT)
-gpio.setup(ena, gpio.OUT)
-
-gpio.setup(trig, gpio.OUT)
-gpio.setup(echo, gpio.IN)
-gpio.setup(button_pin, gpio.IN, pull_up_down = gpio.PUD_DOWN)
-
-gpio.setup(motor_in1, gpio.OUT)
-gpio.setup(motor_in2, gpio.OUT)
-gpio.setup(en, gpio.OUT)
-
-gpio.output(dir, 1)
-gpio.output(motor_in1, gpio.LOW)
-gpio.output(motor_in2, gpio.LOW)
-
-p = gpio.PWM(en, 1000)
-p.start(50)
-
-duration_fwd = 5000
-
-pulse_delay = 0.00001
-step_delay = 0.0000001
-print('Between-step delay set to {} '.format(step_delay))
-
-def distance():
-    # Send out a pulse for 0.00001 s
-    gpio.output(trig, 1)
-    sleep(pulse_delay)
-    gpio.output(trig, 0)
-    
-    start_time = time()
-    stop_time = time()
-    
-    # get the time of the pulse being sent
-    while gpio.input(echo) == 0:
-        start_time = time()
-        
-    # get the time of the pulse returning
-    while gpio.input(echo) == 1:
-        stop_time = time()
-        
-    # find the round-trip time of the pulse
-    elapsed_time = stop_time - start_time
-    
-    # multiply by the speed of sound (34300 cm/s)
-    # and divide by 2 (there and back)
-    
-    return elapsed_time*34300/2
-    
-def forward():
-    gpio.output(ena, gpio.HIGH)
-    #print('Controller enabled')
-    
-    gpio.output(pul, gpio.HIGH)
-    sleep(step_delay)
-    gpio.output(pul, gpio.LOW)
-    sleep(step_delay)
-        
-    gpio.output(ena, gpio.LOW)
-    return None
-
+# Initialize the physical objects in the system 
+elevator = ph.Elevator(motor1 = 23, motor2 = 24, enable = 25, speed = 50)
+button = ph.Button(pin = 14)
+dispenser = ph.Dispenser(pulse = 17, direction = 27, enable = 22)
+ultrasonic = ph.Ultrasonic(trigger = 18, echo = 10, pulse_delay = 0.00001)
+   
 try:
     while True:
-        #dist = distance()
-        #print('Measured distance {} cm'.format(dist))
+        dist = ultrasonic.distance()
         
-        sleep(0.05)
-        
-        #gpio.output(motor_in1, gpio.LOW)
-        #gpio.output(motor_in2, gpio.LOW)
-        
-        if gpio.input(button_pin) == gpio.HIGH:
-            for i in range(2400):
-                forward()
-            #gpio.output(motor_in1, gpio.HIGH)
-            #gpio.output(motor_in2, gpio.LOW)
-            #print("forward")    
+        if dist > 5:
+            print('\nDistance measurements stopped at {} cm'.format(dist))
+            ph.thread_it(elevator)
+            ph.thread_it(button)
+            ph.thread_it(dispenser)
             
+            print('All done, back to measuring distance...')
+
 except KeyboardInterrupt:
     gpio.output(dir, 0)
-        
     gpio.cleanup()
     print('Done')
---
+++ physical_objects.py
@@ -0,0 +1,176 @@
+import time
+
+class Elevator:
+    def __init__(self, motor1 = 23, motor2 = 24, enable = 25, speed = 50):
+        self.motor1 = motor1
+        self.motor2 = motor2
+        self.enable = enable
+        self.ready = threading.Event()
+        self.pins = [motor1, motor2, enable]
+        
+        # Set the speed of the pulse (using Pulse Width Modulation)
+        self.pulse = gpio.PWM(self.enable, 1000)
+        self.pulse.start(self.speed)
+                
+        # Put the elevator pins in output mode
+        gpio.setup(self.motor1, gpio.OUT)
+        gpio.setup(self.motor2, gpio.OUT)
+        gpio.setup(self.enable, gpio.OUT)
+        
+        # Start with both motors OFF
+        gpio.output(self.enable, 0)
+        gpio.output(self.motor1, 0)
+        gpio.output(self.motor2, 0)
+        
+    def __str__(self):
+        return 'Elevator object on pins {}'.format(self.pins)        
+        
+    def run(self):
+        """ Make the elevator go up a certain length, then come back down. """
+        gpio.output(self.enable, 1)
+                
+        gpio.output(self.motor1, 1)
+        gpio.output(self.motor2, 0)
+        print("going up")
+        
+        # Wait 1 second before coming back down
+        time.sleep(1)
+        
+        gpio.output(motor_in1, 0)
+        gpio.output(motor_in2, 1)
+        print("coming back down")
+        
+        time.sleep(1)
+        
+        gpio.output(self.motor1, 0)
+        gpio.output(self.motor2, 0)     
+        
+        print('Round trip finished - back to start position')
+        self.ready.set()
+        
+        gpio.output(self.enable, 0)
+        
+        return 0
+    
+class Button:
+    def __init__(self, pin = 14):
+        self.pin = pin
+        self.ready = threading.Event()
+        
+        # Set up the button pin as a physical input
+        gpio.setup(self.pin, gpio.IN, pull_up_down = gpio.PUD_DOWN)
+        
+    def __str__(self):
+        return 'Button object on pin {}'.format(self.pin)
+    
+    def run(self):
+        """ Wait for the button to be pressed. """
+        
+#         time.sleep(3)
+        while True:
+            if gpio.input(self.pin) == gpio.HIGH:
+                self.ready.set()
+                break
+#         self.ready.set()
+        print('\nButton pressed')
+        return None
+        
+class Dispenser:
+    def __init__(self, pulse = 17, direction = 27, enable = 22, steps = 3600, step_delay = 0.0000001):
+        self.pulse = pulse
+        self.direction = direction
+        self.enable = enable
+        self.steps = steps             # Number of steps needed to rotate the auger
+        self.step_delay = step_delay   # Extremely short delay between steps
+        self.pins = [pulse, direction, enable]
+        
+        self.ready = threading.Event()
+        
+        # Physical pin setup
+        gpio.setup(self.pulse, gpio.OUT)
+        gpio.setup(self.direction, gpio.OUT)
+        gpio.setup(self.enable, gpio.OUT)
+        
+        # Set the direction to FORWARD
+        gpio.output(self.direction, 1)
+        
+        # Start with the stepper motor OFF
+        gpio.output(self.enable, 0)
+        
+    def __str__(self):
+        return 'Dispenser object on pins {}'.format(self.pins)
+    
+    def run(self):
+        """ Dispense candy """
+        
+        print('Rotating auger...')
+
+        for i in range(self.steps):
+            gpio.output(ena, gpio.HIGH)
+            #print('Controller enabled')
+
+            gpio.output(pul, gpio.HIGH)
+            sleep(step_delay)
+            gpio.output(pul, gpio.LOW)
+            sleep(step_delay)
+
+            gpio.output(ena, gpio.LOW)
+#         time.sleep(3)        
+        
+        self.ready.set()
+        print('Candy dispensed')
+        
+        return None
+    
+class Ultrasonic:
+    def __init__(self, trigger = 18, echo = 10, pulse_delay = 0.00001):
+        self.trigger = trigger
+        self.echo = echo
+        self.pulse_delay = pulse_delay
+        self.pins = [trigger, echo, pulse_delay]
+        
+        # Physical pin setup
+        gpio.setup(self.trigger, gpio.OUT)
+        gpio.setup(self.echo, gpio.IN)
+        
+    def __str__(self):
+        return 'Ultrasonic sensor object on pins {}'.format(self.pins)
+    
+    def distance(self):
+        """ Use the ultrasonic sensor to get a distance measurement """
+        
+        # Send out a pulse for 0.00001 s
+        gpio.output(self.trigger, 1)
+        time.sleep(self.pulse_delay)
+        gpio.output(self.trigger, 0)
+
+        start_time = time.time()
+        stop_time = time.time()
+
+        # get the time of the pulse being sent
+        while gpio.input(self.echo) == 0:
+            start_time = time.time()
+
+        # get the time of the pulse returning
+        while gpio.input(self.echo) == 1:
+            stop_time = time.time()
+
+        # find the round-trip time of the pulse
+        elapsed_time = stop_time - start_time
+
+        # multiply by the speed of sound (34300 cm/s)
+        # and divide by 2 (there and back)
+        return elapsed_time*34300/2
+    
+def thread_it(obj, timeout = 10):
+    """ General function to handle threading for the physical components of the system. """
+    
+    thread = threading.Thread(target = obj.run())
+    thread.start()
+    
+    # Run the 'run' function in the obj
+    obj.ready.wait(timeout = timeout)
+    
+    # Clean up
+    thread.join()
+    obj.ready.clear()
