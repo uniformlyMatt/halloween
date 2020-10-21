ed7441effd648f706b4521108781cfc3c79691f4
Matthew Adams <snimpids@gmail.com>
2020-10-21 14:14:06 -0600

    Updated to stepper motor powered elevator

--- physical_objects.py
+++ physical_objects.py
@@ -3,55 +3,77 @@ import threading
 import RPi.GPIO as gpio
 
 class Elevator:
-    def __init__(self, motor1 = 23, motor2 = 24, enable = 25, speed = 50):
-        self.motor1 = motor1
-        self.motor2 = motor2
+    def __init__(self, pulse = 23, direction = 24, enable = 25, timing_belt = 72, step_delay = 0.0000001):
+        self.pulse = pulse
+        self.direction = direction
         self.enable = enable
-        self.speed = speed
-        self.ready = threading.Event()
-        self.pins = [motor1, motor2, enable]
+        self.timing_belt = timing_belt             # Number of steps needed to rotate the auger
+        self.step_delay = step_delay   # Extremely short delay between steps
+        self.pins = [pulse, direction, enable]
         
-        # Put the elevator pins in output mode
-        gpio.setup(self.motor1, gpio.OUT)
-        gpio.setup(self.motor2, gpio.OUT)
+        self.ready = threading.Event()
+        
+        # Physical pin setup
+        gpio.setup(self.pulse, gpio.OUT)
+        gpio.setup(self.direction, gpio.OUT)
         gpio.setup(self.enable, gpio.OUT)
         
-        # Set the speed of the pulse (using Pulse Width Modulation)
-        self.pulse = gpio.PWM(self.enable, 1000)
-        self.pulse.start(self.speed)       
+        # Set the direction to FORWARD
+        gpio.output(self.direction, 1)
         
-        # Start with both motors OFF
+        # Start with the stepper motor OFF
         gpio.output(self.enable, 0)
-        gpio.output(self.motor1, 0)
-        gpio.output(self.motor2, 0)
         
     def __str__(self):
-        return 'Elevator object on pins {}'.format(self.pins)        
+        return 'Elevator object on pins {}'.format(self.pins)
+    
+    def up(self):
+        """ Make the elevator go up a certain length. """
+        gpio.output(self.direction, 1)
+        
+        for i in range(self.timing_belt):
+            gpio.output(self.enable, 1)
+            #print('Controller enabled')
+
+            gpio.output(self.pulse, 1)
+            time.sleep(self.step_delay)
+            gpio.output(self.pulse, 0)
+            time.sleep(self.step_delay)
+
+            gpio.output(self.enable, 0)
+#         time.sleep(3)        
+        
+        return None
+    
+    def down(self):
+        """ Make the elevator come down a certain length. """
+        
+        gpio.output(self.direction, 0)
+        
+        for i in range(self.timing_belt):
+            gpio.output(self.enable, 1)
+            #print('Controller enabled')
+
+            gpio.output(self.pulse, 1)
+            time.sleep(self.step_delay)
+            gpio.output(self.pulse, 0)
+            time.sleep(self.step_delay)
+
+            gpio.output(self.enable, 0)
+#         time.sleep(3)        
+        
+        return None
         
     def run(self):
         """ Make the elevator go up a certain length, then come back down. """
-        gpio.output(self.enable, 1)
-                
-        gpio.output(self.motor1, 1)
-        gpio.output(self.motor2, 0)
-        print("going up")
+        self.up()
         
-        # Wait 1 second before coming back down
-        time.sleep(1)
+        # Wait 2 seconds at the top
+        time.sleep(2)
         
-        gpio.output(self.motor1, 0)
-        gpio.output(self.motor2, 1)
-        print("coming back down")
+        self.down()
         
-        time.sleep(1)
-        
-        gpio.output(self.motor1, 0)
-        gpio.output(self.motor2, 0)     
-        
-        print('Round trip finished - back to start position')
         self.ready.set()
-        
-        gpio.output(self.enable, 0)
         
         return 0
     
