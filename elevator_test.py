6c47f6e44a0fa3378023b7680c43f4578caf896e
uniformlyMatt <snimpids@gmail.com>
2020-10-29 23:52:42 -0600

    Updated elevator_test

--- elevator_test.py
+++ elevator_test.py
@@ -5,7 +5,7 @@ import physical_objects as ph
 gpio.setwarnings(False)
 gpio.setmode(gpio.BCM)
 
-elevator = ph.Stepper(pulse = 13, direction = 19, enable = 26, steps = 8000, name = 'Elevator')
+elevator = ph.Stepper(pulse = 13, direction = 19, enable = 26, steps = 56000, name = 'Elevator')
 
 print(elevator)
 
