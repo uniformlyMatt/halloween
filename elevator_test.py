ae2bd1ab0316d69888daa283682468343ace1b1a
uniformlyMatt <snimpids@gmail.com>
2020-10-29 22:31:18 -0600

    Updated elevator_test.py

--- elevator_test.py
+++ elevator_test.py
@@ -5,24 +5,24 @@ import physical_objects as ph
 gpio.setwarnings(False)
 gpio.setmode(gpio.BCM)
 
-elevator = ph.Stepper(pulse = 13, direction = 19, enable = 26, steps = 56000, name = 'Elevator')
+elevator = ph.Stepper(pulse = 13, direction = 19, enable = 26, steps = 8000, name = 'Elevator')
 
 print(elevator)
 
 # Run the test
 
 # Send the elevator up
-#elevator.reverse()
+print("Going up")
+elevator.reverse()
 ph.thread_it(elevator)
-##print(elevator.fwd)
-time.sleep(0.5)
+
+time.sleep(3)
 
 # Change the elevator direction and send it back down
 
-print(elevator.bwd)
-#elevator.reverse()
-##time.sleep(0.5)
-#ph.thread_it(elevator)
+print("Coming back down")
+elevator.reverse()
+ph.thread_it(elevator)
 
 gpio.cleanup()
 print('Done')
